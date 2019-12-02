f4464b9b16d4e4b4097bf559c667c8dcbd5a8721
owinolawrence <owinolawrence21@gmail.com>
2019-12-02 22:18:20 +0300

    create a manager shell decorator

---
+++ manage.py
@@ -0,0 +1,28 @@
+from flask_script import Manager, Server
+from app import app ,db
+from app import create_app,db
+from app.models import User,Post,Comment
+from flask_migrate import Migrate,MigrateCommand
+
+
+# app = create_app('development')
+# manager =  Manager(app)
+# manager.add_command('server',Server(use_debugger=True))
+
+# migrate = Migrate(app,db)
+# manager.add_command('db',MigrateCommand)
+
+
+
+@manager.shell
+def add_shell_context():
+    return {'db': db, 'User':User, 'Post':Post, 'Comment':Comment}
+
+# @manager.command
+# def test():
+#     import unittest
+#     test=unittest.TestLoader().discover("Test")
+#     unittest.TextTestRunner(verbosity=5).run(test)
+
+# if __name__== "__main__":
+#     manager.run()
