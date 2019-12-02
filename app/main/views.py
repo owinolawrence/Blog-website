c785817ca47747927494acf8f5f5d9c8bba63bd8
owinolawrence <owinolawrence21@gmail.com>
2019-12-02 22:50:28 +0300

    create index route decorator

--- app/main/views.py
+++ app/main/views.py
@@ -1,2 +1,17 @@
 from flask import Flask,render_template , url_for,request,redirect
 from . import main
+from flask_login import login_required,current_user
+from app.models import User,Post,Comment
+from .. import db,photos
+from app.requests import get_Quotes
+
+
+
+@main.route('/')
+def index():
+    quotes=get_Quotes()
+    print(quotes)
+   
+    return render_template('index.html',quotes=quotes)
+
+
